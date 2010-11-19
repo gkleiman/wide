85a3d92f474f6fcadfdd8dca2252fda9fe14f321
Gastón Kleiman <gaston.kleiman@gmail.com>
2010-11-18 22:22:28 -0300

    Added specs for Project.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "to_param should return the project name" do
+    p = Factory.build(:project)
+
+    p.to_param.should == p.name
+  end
+
+  it "should set the repository path after validation" do
+    p = Factory.build(:project)
+
+    p.repository = p.build_repository
+    p.repository.scm = "Mercurial"
+
+    p.valid?
+
+    p.repository.path.should == Wide::PathUtils.secure_path_join(Settings.repositories_base, File.join(p.user.user_name, p.name))
+  end
 end
