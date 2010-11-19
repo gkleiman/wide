d239e7c5e7a0cfd373b421edc6c039029d6e5ee9
Gastón Kleiman <gaston.kleiman@gmail.com>
2010-11-18 22:22:55 -0300

    Fixed MercurialAdapter specs.

--- spec/lib/wide/scm/adapters/mercurial_adapter_spec.rb
+++ spec/lib/wide/scm/adapters/mercurial_adapter_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 describe Wide::Scm::Adapters::MercurialAdapter do
   before(:each) do
+    FileUtils.rm_rf(Rails.root.join('tmp', 'scm_tests'))
     FileUtils.mkdir_p(Rails.root.join('tmp', 'scm_tests'))
-    FileUtils.rm_rf(Rails.root.join('tmp', 'scm_tests', 'hg_test'))
-    FileUtils.cp_r(Rails.root.join('spec', 'fixtures', 'hg_test'), Rails.root.join('tmp', 'scm_tests'))
-
-    @scm = Wide::Scm::Adapters::MercurialAdapter.new(Rails.root.join('tmp', 'scm_tests', 'hg_test').to_s)
   end
 
   it "should contain .hg in its skip_paths" do
@@ -14,6 +11,10 @@ describe Wide::Scm::Adapters::MercurialAdapter do
   end
 
   it "should return a hash with the status of the repository" do
+    FileUtils.cp_r(Rails.root.join('spec', 'fixtures', 'hg_test'), Rails.root.join('tmp', 'scm_tests'))
+
+    @scm = Wide::Scm::Adapters::MercurialAdapter.new(Rails.root.join('tmp', 'scm_tests', 'hg_test').to_s)
+
     status = @scm.status
 
     status[Wide::PathUtils.secure_path_join(@scm.base_path, '/added_file')].should == [ :added ]
@@ -22,6 +23,11 @@ describe Wide::Scm::Adapters::MercurialAdapter do
   end
 
   it "should be able to init an empty repository" do
+    FileUtils.mkdir_p(Rails.root.join('tmp', 'scm_tests', 'new_repo'))
+
+    @scm = Wide::Scm::Adapters::MercurialAdapter.new(Rails.root.join('tmp', 'scm_tests', 'new_repo').to_s)
+
+    @scm.init.should == true
   end
 
   it "should only allow remote repositories urls" do
