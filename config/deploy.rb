ff688e2d709a7db859b2575255c40563660d3dda
Gaston Kleiman <gaston.kleiman@gmail.com>
2010-12-29 05:30:08 -0300

    Deployment recipe improved.

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,30 +15,17 @@ role :web, "192.168.0.102"                          # Your HTTP server, Apache/e
 role :app, "192.168.0.102"                          # This may be the same as your `Web` server
 role :db,  "192.168.0.102", :primary => true # This is where Rails migrations will run
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 namespace :customs do
-  task :symlinks, :roles => :app do
-    run <<-CMD
-      mkdir -p #{deploy_to}/#{shared_dir}/repositories
-    CMD
-    run <<-CMD
-      ln -nfs #{deploy_to}/#{shared_dir}/repositories #{deploy_to}/#{current_dir}/public/repositories
-    CMD
-    run <<-CMD
-      mkdir -p #{deploy_to}/#{shared_dir}/compiled
-    CMD
-    run <<-CMD
-      ln -nfs #{deploy_to}/#{shared_dir}/compiled #{deploy_to}/#{current_dir}/public/compiled
-    CMD
-    run <<-CMD
-      mkdir -p #{deploy_to}/#{shared_dir}/db && touch #{deploy_to}/#{shared_dir}/db/production.sqlite3
-    CMD
-    run <<-CMD
-      ln -nfs #{deploy_to}/#{shared_dir}/db/production.sqlite3 #{deploy_to}/#{current_dir}/db/production.sqlite3
-    CMD
+  task :setup, :except => { :no_release => true } do
+    run "#{try_sudo} mkdir -p #{deploy_to}/#{shared_dir}/repositories"
+    run "#{try_sudo} mkdir -p #{deploy_to}/#{shared_dir}/compiled"
+    run "#{try_sudo} mkdir -p #{deploy_to}/#{shared_dir}/db && #{try_sudo} touch #{deploy_to}/#{shared_dir}/db/production.sqlite3"
+  end
+
+  task :symlink, :except => { :no_release => true } do
+    run "ln -nfs #{deploy_to}/#{shared_dir}/repositories #{deploy_to}/#{current_dir}/public/repositories"
+    run "ln -nfs #{deploy_to}/#{shared_dir}/compiled #{deploy_to}/#{current_dir}/public/compiled"
+    run "ln -nfs #{deploy_to}/#{shared_dir}/db/production.sqlite3 #{deploy_to}/#{current_dir}/db/production.sqlite3"
   end
 end
 
@@ -50,7 +37,8 @@ namespace :deploy do
   end
 end
 
-after "deploy:symlink", "customs:symlinks"
+after "deploy:setup", "customs:setup"
+after "deploy:symlink", "customs:symlink"
 after "deploy:stop", "delayed_job:stop"
 after "deploy:start", "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
