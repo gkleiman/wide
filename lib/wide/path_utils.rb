16733b682a0855b4a9fdc4dd9b87fbb02f02eeb5
Gaston Kleiman <gaston.kleiman@gmail.com>
2010-12-30 07:59:23 -0300

    Wide::PathUtils.secure_path_join now takes varargs.

--- lib/wide/path_utils.rb
+++ lib/wide/path_utils.rb
@@ -24,17 +24,19 @@ module Wide
         (path != '/' && path[-1,1] == "/") ? path[0..-2] : path
       end
 
-      def secure_path_join(base, path = '/')
+      def secure_path_join(base, *path)
         raise BaseDirectoryTraversalError.new("You need a base path to secure join a path") if base.blank?
 
-        joined_path = File.expand_path(File.join([base.to_s, path.to_s]))
+        path = '/' if path.to_s.blank?
+
+        joined_path = File.expand_path(File.join(base.to_s, *path))
         joined_path = without_trailing_slash(joined_path)
 
         base = without_trailing_slash(File.expand_path(base.to_s))
         # Raise an exception if the expanded path is not inside the base path
         unless base == '/' || joined_path == base || joined_path.index(base + File::SEPARATOR) == 0
           exception = BaseDirectoryTraversalError.new("Wrong path join: base=#{base} path=#{path} result=#{joined_path}")
-          logger.debug exception.to_s if logger && logger.debug?
+          logger.debug(exception.to_s) if logger && logger.debug?
           raise exception
         end
 
