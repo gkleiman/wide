7c77a153fb8c6ac599613eb8c8f9c8619f5c21f3
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-10 07:06:04 -0300

    Made compilation's ajax error reporting more robust.

--- public/javascripts/compile.js
+++ public/javascripts/compile.js
@@ -1,28 +1,39 @@
 WIDE.compile = (function () {
+  var POLL_TIME = 3000;
   var compilation_in_progress = false;
+  var compilation_start_time = 0;
+
+  var error_handler = function (xhr, textStatus, errorThrown) {
+    compilation_in_progress = false;
+    WIDE.notifications.error('Compilation failed.');
+    $('#compile_button').button('option', 'disabled', false);
+  };
+
+  $(document).ajaxError(function (e, xhr, settings, exception) {
+    if (settings.url == WIDE.base_path() + '/compiler_output') {
+      error_handler();
+    }
+  });
 
   return {
     poll_compiler_output: function (timestamp) {
       var perform_poll = function () {
         $.getJSON(WIDE.base_path() + '/compiler_output', function (response) {
           if(!response || !response.success) {
-            compilation_in_progress = false;
-
-            WIDE.notifications.error('Compilation failed.');
-
-            return false;
+            error_handler();
           }
 
           if(response.compile_status.updated_at > timestamp) {
             compilation_in_progress = false;
 
-            $('#compile_button').button('option', 'disabled', false);
             WIDE.notifications.success('Compilation finished.');
 
             WIDE.compiler_output.clear();
             for(var i = 0; i < response.compile_status.output.length; ++i) {
               WIDE.compiler_output.add_output(response.compile_status.output[i]);
             }
+
+            $('#compile_button').button('option', 'disabled', false);
 
             if(response.compile_status.status === 'success') {
               document.location.href = encodeURI(WIDE.base_path() + '/download_binary');
@@ -32,24 +43,26 @@ WIDE.compile = (function () {
               });
             }
           } else {
-            setTimeout(function () {
-              WIDE.compile.poll_compiler_output(timestamp)
-            }, 1000);
+            // Consider the compilation as timed out after two minutes.
+            if ((new Date().getTime() - compilation_start_time) > 120000) {
+              error_handler();
+            } else {
+              setTimeout(function () {
+                perform_poll();
+              }, POLL_TIME);
+            }
           }
         });
       };
 
+      compilation_start_time = new Date().getTime();
       compilation_in_progress = true;
       perform_poll();
     },
     is_compilation_in_progress: function () {
-      return compilation_in_progress == true;
+      return compilation_in_progress === true;
     },
     compile: function () {
-      var error_handler = function (xhr, textStatus, errorThrown) {
-        WIDE.notifications.error('Compilation failed.');
-      };
-
       $('#compile_button').button('option', 'disabled', true).mouseout().blur();
       WIDE.compiler_output.clear();
       WIDE.notifications.activity_started('Compiling ...');
