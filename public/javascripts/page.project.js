0ae3e6bcf17a1c1fa93f2cfa9663765487baf7f5
Gastón Kleiman <gaston.kleiman@gmail.com>
2011-01-18 06:50:11 -0300

    Extract js from page.project.js to page.project.edit.js.

--- public/javascripts/page.project.js
+++ public/javascripts/page.project.js
@@ -24,13 +24,6 @@ $(function () {
     });
   }
 
-  $('.remove_link').live('click', function () {
-    $(this).prev("input[type=hidden]").val("1");
-    $(this).closest(".fields").hide();
-
-    return false;
-  });
-
   $('table').delegate('tr.ui-state-default', 'hover', function () {
     $(this).toggleClass('ui-state-hover');
     return true;
@@ -47,28 +40,5 @@ $(function () {
     event.stopPropagation();
 
     return false;
-  });
-
-  $('.remove_collaborator').live('click', function () {
-    $(this).parent().fadeOut('slow', function () { $(this).remove(); });
-  });
-
-  $('#collaborator_ids').tokenInput("/users", {
-    hintText: "Begin typing the user name of the collaborator you wish to add.",
-    noResultsText: "No results",
-    searchingText: "Searching...",
-    prePopulate: collaborators_ids,
-    classes: {
-      tokenList: "token-input-list ui-widget ui-widget-content",
-      token: "ui-widget-header ui-state-default",
-      tokenDelete: "ui-icon ui-icon-circle-minus",
-      selectedToken: "ui-state-active",
-      highlightedToken: "ui-state-active",
-      dropdown: "ui-autocomplete ui-menu ui-widget ui-widget-content ui-corner-all autocomplete-dropdown",
-      dropdownItem: "ui-menu-item ui-corner-all",
-      dropdownItem2: "",
-      selectedDropdownItem: "ui-menu-item ui-corner-all ui-state-hover",
-      inputToken: "ui-autocomplete-input"
-    }
   });
 });
