6f4af3f95b61b5a027b82514218c45b147303dc3
Gastón Kleiman <gaston.kleiman@gmail.com>
2011-01-19 15:57:09 -0300

    Only initialize the SCMs engines in development.

--- config/initializers/scm.rb
+++ config/initializers/scm.rb
@@ -1,2 +1,4 @@
-Wide::Scm::Scm.add_adapter('Mercurial')
-Wide::Scm::Scm.add_adapter('Filesystem')
+if Rails.env == 'development'
+  Wide::Scm::Scm.add_adapter('Mercurial')
+  Wide::Scm::Scm.add_adapter('Filesystem')
+end
