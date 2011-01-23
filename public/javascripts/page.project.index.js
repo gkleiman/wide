9639806fcc27f30975113ae81af1b439c8aa4717
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-22 22:05:52 -0300

    Reorganize and document js.

--- public/javascripts/page.project.index.js
+++ public/javascripts/page.project.index.js
@@ -1,9 +1,25 @@
+// Refreshes the projects table until all projects are initialized
 var poll_projects_table = function () {
-  if ($('.initializing').length > 0) {
+  if ($('.initializing').length > 0) .{
     setTimeout(function () { $.getScript(this.href); return true; }, 2000);
   }
 };
 
 $(function () {
+  // Make rows of the projects table clickable
+  $('#projects table tbody tr.success').live('click', function (event) {
+    if (event.target.nodeName === 'A') {
+      return true;
+    }
+
+    window.location = $(this).find('a:eq(0)').attr('href');
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    return false;
+  });
+
+  // Refresh the projects table if there is any uninitialized project
   poll_projects_table();
 });
--- public/javascripts/page.project.js
+++ public/javascripts/page.project.js
@@ -4,16 +4,4 @@ $(function () {
     return true;
   });
 
-  $('#projects table tbody tr.success').live('click', function (event) {
-    if (event.target.nodeName === 'A') {
-      return true;
-    }
-
-    window.location = $(this).find('a:eq(0)').attr('href');
-
-    event.preventDefault();
-    event.stopPropagation();
-
-    return false;
-  });
 });
