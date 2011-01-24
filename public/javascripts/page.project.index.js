c0773e6b96b8094db2a3de315c0a4a17405e39c5
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-23 21:57:02 -0300

    Remove extra char that breaks js.

--- public/javascripts/page.project.index.js
+++ public/javascripts/page.project.index.js
@@ -1,6 +1,6 @@
 // Refreshes the projects table until all projects are initialized
 var poll_projects_table = function () {
-  if ($('.initializing').length > 0) .{
+  if ($('.initializing').length > 0) {
     setTimeout(function () { $.getScript(this.href); return true; }, 2000);
   }
 };
