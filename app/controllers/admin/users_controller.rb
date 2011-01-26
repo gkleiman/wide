683fd46eedba70b437e59e4aecf5ece87e75a704
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-26 17:56:16 -0300

    Fix editing of users in the admin panel.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,18 @@
 class Admin::UsersController < Admin::ResourcesController
+  def update
+    params[@object_name].delete(:password) if params[@object_name][:password].blank?
+    params[@object_name].delete(:password_confirmation) if params[@object_name][:password_confirmation].blank?
+
+    respond_to do |format|
+      if @item.update_attributes(params[@object_name])
+        set_attributes_on_update
+        reload_locales
+        format.html { redirect_on_success }
+        format.json { render :json => @item }
+      else
+        format.html { render :edit }
+        format.json { render :json => @item.errors.full_messages }
+      end
+    end
+  end
 end
