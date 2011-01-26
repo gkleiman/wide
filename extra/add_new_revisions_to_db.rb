bc82f2eaefa518c7b63c651b403b5842007ae986
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-26 20:57:44 -0300

    Fix path to rails script.

--- extra/add_new_revisions_to_db.rb
+++ extra/add_new_revisions_to_db.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env /Users/gkleiman/Documents/facu/wide/script/rails runner
+#!/usr/bin/env /home/wide/application/current/script/rails runner
 
 repo_absolute_path = Dir.getwd
 repo_path = Wide::PathUtils.relative_to_base(Settings.repositories_base, repo_absolute_path)
