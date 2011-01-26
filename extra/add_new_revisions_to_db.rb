36c482fd6266b4c3abb1d4c516a127d0c4c75619
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-26 20:45:47 -0300

    Add new revisions to the db after a push/pull/bundle.

---
+++ extra/add_new_revisions_to_db.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env /Users/gkleiman/Documents/facu/wide/script/rails runner
+
+repo_absolute_path = Dir.getwd
+repo_path = Wide::PathUtils.relative_to_base(Settings.repositories_base, repo_absolute_path)
+
+Repository.find_by_path(repo_path).add_new_revisions_to_db
