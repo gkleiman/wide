19955fe1a5d616447a23bbe1e052c44caa369701
Gaston Kleiman <gaston.kleiman@gmail.com>
2011-01-27 00:25:52 -0300

    Remove forgotten debugging code.

--- extra/add_new_revisions_to_db.rb
+++ extra/add_new_revisions_to_db.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'production'
 require '/home/wide/application/current/config/environment.rb'
 
 repo_absolute_path = Dir.getwd
-raise "#{repo_absolute_path} esta en #{Settings.repositories_base} ??"
+
 repo_path = Wide::PathUtils.relative_to_base(Settings.repositories_base, repo_absolute_path)
 
 Repository.find_by_path(repo_path).add_new_revisions_to_db
